abf64245dc94781d644c9dd3dc4800ebf0e460a8
vvmsecured <vvmspace@gmail.com>
2018-10-23 02:02:52 +0500

    PlusSortCommand

---
+++ app/Commands/PlusSortCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Commands;
+
+use Illuminate\Console\Scheduling\Schedule;
+use LaravelZero\Framework\Commands\Command;
+
+class PlusSortCommand extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'plussort {s}';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    static function PlusSort($s){
+        $array = explode('+', $s);
+        sort($array);
+        return implode('+', $array);
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->info(static::PlusSort($this->argument('s')));
+    }
+
+    /**
+     * Define the command's schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
+     * @return void
+     */
+    public function schedule(Schedule $schedule): void
+    {
+        // $schedule->command(static::class)->everyMinute();
+    }
+}
